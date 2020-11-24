08b7b6cb031d7d88ac896cf8b09551ea14bf1d2f
Archi Shende <shendearchi5@gmail.com>
2020-11-24 21:21:05 +0530

    abcd

--- WebIdentityDemo/Startup.cs
+++ WebIdentityDemo/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(WebIdentityDemo.Startup))]
 namespace WebIdentityDemo
 {
+    //i hate dotnet
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
