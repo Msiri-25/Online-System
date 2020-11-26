e62d96ca8164b6ae6e24c019026a24762758bc65
Archi Shende <shendearchi5@gmail.com>
2020-11-26 19:57:34 +0530

    just added a msg

--- WebIdentityDemo/Models/IdentityModels.cs
+++ WebIdentityDemo/Models/IdentityModels.cs
@@ -16,6 +16,11 @@ namespace WebIdentityDemo.Models
     // You can add User data for the user by adding more properties to your User class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// hey there just stay calm
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <returns></returns>
         public ClaimsIdentity GenerateUserIdentity(ApplicationUserManager manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
